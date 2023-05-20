d39e0d9fd12a0bba559171f625fe82556de0d1a1
JennaStrom <123525191+JennaStrom@users.noreply.github.com>
2023-05-20 15:17:34 -0500

    The notes are saving to the left hand side

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -6,4 +6,15 @@ router.get('/notes', (req, res) =>
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
+router.post('/notes', (req, res) => {
+    const newNote = req.body
+readAndAppend(newNote, './db/db.json');
+});
+
+// router.delete('/notes/:id', (req, res) => {
+//     deleteNote(notes, req.params.id);
+//     res.json(notes);
+// })
+
+
 module.exports = router;
